4fa5b6f80d2e0a9724ba536392cecc2c04faabcf
Michael Alarcon Carrillo <michaelalarcon2005@gmail.com>
2025-05-23 12:53:19 -0700

    Nuestros paquetes front end

--- src/views/Calendario.tsx
+++ src/views/Calendario.tsx
@@ -2,21 +2,18 @@ import Calendario20 from "../components/Calendario20";
 
 export default function Calendario() {
   return (
-    <div className="bg-[#ebebd3] w-full min-h-screen flex  justify-center">
-      <div className="text-black font-body text-center">
-        <p className="text-4xl md:text-6xl leading-snug">
-          !EMPECEMOS¡
+    <div className="bg-[#ebebd3] w-full min-h-screen flex items-top justify-center">
+      <div className="text-black font-body text-center space-y-6">
+        <p className="text-4xl md:text-6xl leading-snug font-bold">
+          ¡EMPECEMOS!
         </p>
-        <div className="mt-1">
-          <p className="text-4xl md:text-1xl leading-snug">
-            ¿Que fecha es tu evento?
-          </p>
-        </div>
-        <div className=" justify-center items-center">
+        <p className="text-2xl md:text-3xl leading-snug">
+          ¿Qué fecha es tu evento?
+        </p>
+        <div className="flex justify-center">
           <Calendario20 />
         </div>
-
       </div>
     </div>
-  )
+  );
 }
