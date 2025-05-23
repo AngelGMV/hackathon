497523623b4bfa2cfc3b54c0b4e554eeb9feed57
Michael Alarcon Carrillo <michaelalarcon2005@gmail.com>
2025-05-23 10:33:35 -0700

    HOLA

---
+++ src/views/Inicio.tsx
@@ -0,0 +1,19 @@
+
+export default function Inicio() {
+  return (
+    <div className="bg-[#ebebd3] w-full min-h-screen flex items-center justify-center">
+      <div className="text-black font-body text-center">
+        <p className="text-4xl md:text-6xl leading-snug">
+          EL BANQUETE QUE <br />
+          TU EVENTO MERECE
+        </p>
+
+        <p className="text-xl md:text-2xl mt-10">
+          El mejor evento <br />
+          empieza con un clic
+        </p>
+      </div>
+    </div>
+
+  )
+}
---
+++ src/views/MisEventos.tsx
@@ -0,0 +1,8 @@
+
+export default function MisEventos() {
+  return (
+    <div className="bg-[#ebebd3]">
+      HOLA
+    </div>
+  )
+}
